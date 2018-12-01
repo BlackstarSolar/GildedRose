109cf7dd8b0c00e8acf9a01e6f58045094c69fde
Graham King <graham.king@gmail.com>
2018-12-01 19:49:16 +0000

    Added sulfuras tests

---
+++ src/GildedRose.Tests/SulfurasTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using GildedRose.Console;
+using Xunit;
+
+namespace GildedRose.Tests
+{
+    public class SulfurasTests : ProgramTests
+    {
+        [Fact]
+        public void Never_has_to_be_sold()
+        {
+            var item = new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 1, Quality = 10 };
+
+            Items.Add(item);
+
+            Target.UpdateQuality();
+
+            item.SellIn.Should().Be(1);
+        }
+
+        [Fact]
+        public void Does_not_reduce_in_quality()
+        {
+            var item = new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 1, Quality = 10 };
+
+            Items.Add(item);
+
+            Target.UpdateQuality();
+
+            item.Quality.Should().Be(10);
+        }
+    }
+}
